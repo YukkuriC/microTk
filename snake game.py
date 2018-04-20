f2ef2211ca63c0d0878ef4e84e70f1bb272c20f1
YukkuriC <34098703+YukkuriC@users.noreply.github.com>
2018-04-20 23:48:54 +0800

    test case for button input; a greedy-snake game
    improves visual performance;
    fixed a bug when score reaches 25 (and added winning screen)

--- snake game.py
+++ snake game.py
@@ -3,17 +3,30 @@ from random import randint
 
 
 def SNAKE():
+    # ============ GAME DATA ============
+    class SNAKE_data:
+        def __init__(SNAKE):
+            SNAKE.head = [0, 0]
+            SNAKE.GOTO = [[1, 0, -1, 0], [0, 1, 0, -1]]
+            SNAKE.GAME_RUNNING = True
+            SNAKE.CURR_DIRECTION = 0
+            SNAKE.SCORE = 1
+            SNAKE.body = [[0, 0]]
+            SNAKE.pt = [0, 0]
+            while SNAKE.pt == SNAKE.head:
+                SNAKE.pt = [randint(0, 4), randint(0, 4)]
+
     # ============ FUNCTIONS ============
     # update display
     def update_frame(frame):
-        bitmap = Image()
+        bitmap = Image(5, 5)
         for i in range(len(SNAKE.body)):
             bodypart = SNAKE.body[i]
-            lightness = 7 - int(3 * (i + 1) / len(SNAKE.body))
-            bitmap.set_pixel(*bodypart, lightness)
-        bitmap.set_pixel(*SNAKE.head, 9)
-        if frame // 5 == 0:
-            bitmap.set_pixel(*SNAKE.pt, 9)
+            lightness = int(7 - (4 * (i + 1) / len(SNAKE.body)))
+            bitmap.set_pixel(bodypart[0], bodypart[1], lightness)
+        bitmap.set_pixel(SNAKE.head[0], SNAKE.head[1], 8)
+        if frame:
+            bitmap.set_pixel(SNAKE.pt[0], SNAKE.pt[1], 9 - 2 * abs(5 - frame))
         display.show(bitmap)
         sleep(50)
 
@@ -46,6 +59,12 @@ def SNAKE():
         # eat & regenerate point
         if SNAKE.pt == SNAKE.head:
             SNAKE.SCORE += 1
+            # end gmae
+            if SNAKE.SCORE == 25:
+                SNAKE.GAME_RUNNING = None
+                return
+
+            # generate new point
             occupied = [[False] * 5 for i in range(5)]
             occupied[SNAKE.head[0]][SNAKE.head[1]] = True
             for bodypart in SNAKE.body:
@@ -61,38 +80,43 @@ def SNAKE():
     def gameover_screen():
         update_frame(0)
         for i in range(2):
-            display.set_pixel(*SNAKE.head, 0)
+            display.set_pixel(SNAKE.head[0], SNAKE.head[1], 0)
             sleep(100)
-            display.set_pixel(*SNAKE.head, 9)
+            display.set_pixel(SNAKE.head[0], SNAKE.head[1], 9)
             sleep(100)
-        display.set_pixel(*SNAKE.pt, 0)
+        display.set_pixel(SNAKE.pt[0], SNAKE.pt[1], 0)
         while SNAKE.body:
-            display.set_pixel(*SNAKE.body[0], 0)
+            display.set_pixel(SNAKE.body[0][0], SNAKE.body[0][1], 0)
             SNAKE.body.pop(0)
             sleep(100)
         display.scroll('SCORE:%d' % SNAKE.SCORE)
 
+    def clear_screen():
+        update_frame(0)
+        while not (button_a.get_presses() or button_b.get_presses()):
+            SNAKE.body.insert(0, SNAKE.head)
+            SNAKE.head = SNAKE.body.pop()
+            update_frame(0)
+        display.scroll('GAMECLEAR')
+
     # ============ MAIN LOGIC ============
     # init
-    SNAKE.head = [0, 0]
-    SNAKE.GOTO = [[1, 0, -1, 0], [0, 1, 0, -1]]
-    SNAKE.GAME_RUNNING = True
-    SNAKE.CURR_DIRECTION = 0
-    SNAKE.SCORE = 1
-    SNAKE.body = [[0, 0]]
-    SNAKE.pt = [0, 0]
-    while SNAKE.pt == SNAKE.head:
-        SNAKE.pt = [randint(0, 4), randint(0, 4)]
+    SNAKE = SNAKE_data()
 
     # main process
     frame = 0
+    button_a.get_presses()
+    button_b.get_presses()
     while SNAKE.GAME_RUNNING:
         update_frame(frame)
         frame += 1
         if frame >= 10:
             frame -= 10
             game_logic()
-    gameover_screen()
+    if SNAKE.GAME_RUNNING is None:
+        clear_screen()
+    else:
+        gameover_screen()
 
 
 display.scroll('SNAKE', 75)
